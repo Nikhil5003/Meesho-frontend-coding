f5b76de38d43b83e21efacae449603f4a78ec83e
Nikhil5003 <nikhil9812382543@gmail.com>
2023-11-28 17:52:05 +0530

    infinte-scroll commit

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,41 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import fetchData from "../fetchData";
-import Card from "./Card";
-import Pagination from "./Pagination";
+// import { FiCommand } from "react-icons/fi";
+import { Audio } from "react-loader-spinner";
+import "./components.css";
+// import Card from "./Card";
+// import Pagination from "./Pagination";
 const Container = () => {
   const [items, setItems] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const items_per_page = 2;
-  const totalPageCount = Math.ceil(items.length / items_per_page);
-  const startingIndex = (currentPage - 1) * items_per_page;
-  const endingIndex = currentPage * items_per_page;
-  const data = items.slice(startingIndex, endingIndex);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const limit = 10;
+  const lastElementRef = useRef(null);
+  console.log(lastElementRef);
+  const handleObserver = useCallback(
+    ([entry]) => {
+      const { isIntersecting } = entry;
+      if (isIntersecting && !loading) {
+        setCurrentPage(currentPage === limit ? currentPage : currentPage + 1);
+      }
+    },
+    [loading]
+  );
   useEffect(() => {
-    fetchData().then((res) => {
-      console.log(res);
-      setItems(res);
+    fetchData(currentPage, limit, setLoading).then((res) => {
+      setItems((prev) => [...prev, ...res]);
     });
-  }, []);
+  }, [currentPage]);
+  // const items_per_page = 2;
+  // const totalPageCount = Math.ceil(items.length / items_per_page);
+  // const startingIndex = (currentPage - 1) * items_per_page;
+  // const endingIndex = currentPage * items_per_page;
+  // const data = items.slice(startingIndex, endingIndex);
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver);
+    if (lastElementRef && lastElementRef.current) {
+      observer.observe(lastElementRef.current);
+    }
+    console.log("i am called");
+    return () => observer.disconnect();
+  }, [lastElementRef, handleObserver]);
 
   return (
-    <div className="container">
-      {data.map((ele) => {
-        const { price, thumbnail, description, title } = ele;
-        return (
-          <Card
-            price={price}
-            image={thumbnail}
-            description={description}
-            title={title}
-          />
-        );
-      })}
-      <Pagination
+    <>
+      <div className="container">
+        {items.map((ele, index) => {
+          const { price, thumbnail, description, title } = ele;
+          return (
+            // <Card
+            //   lastElementRef={items.length === index + 1 ? lastElementRef : null}
+            //   price={price}
+            //   image={thumbnail}
+            //   description={description}
+            //   title={title}
+            // />
+            <div
+              ref={items.length === index + 1 ? lastElementRef : null}
+              className="cardClass"
+            >
+              <h1 className="description">title : {title}</h1>
+              <img src={thumbnail} alt="" />
+              <h1 className="description">description : {description}</h1>
+              <h1>price :{price}</h1>
+            </div>
+          );
+        })}
+
+        {/* <Pagination
         setCurrentPage={setCurrentPage}
         totalPageCount={totalPageCount}
         currentPage={currentPage}
-      />
-    </div>
+      /> */}
+        {/* <div ref={lastElementRef}></div> */}
+      </div>
+      {loading && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass="wrapperClass"
+        />
+      )}
+    </>
   );
 };
 
--- src/fetchData.jsx
+++ src/fetchData.jsx
@@ -1,9 +1,13 @@
-const api_url = "https://dummyjson.com/products?limit=10&skip=0";
-const fetchData = async () => {
+const fetchData = async (skip, limit, setLoading) => {
+  const api_url = `https://dummyjson.com/products?limit=${limit}&skip=${
+    (skip - 1) * 10
+  }`;
   try {
+    setLoading(true);
     const response = await fetch(api_url);
     let data = await response.json();
     console.log(data);
+    setLoading(false);
     let { products = [] } = data || {};
     products = products.map(({ title, price, description, thumbnail }) => {
       return {
